9768c5fb20a67d0429d506ff501c98f8fae1c29a
YaelAguilar <arcadencraft@gmail.com>
2026-02-25 16:31:56 -0600

    Add scroll-to-top functionality on App component mount

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navbar } from './components/Navbar';
 import { GridBackground } from './components/GridBackground';
 import { Hero } from './sections/Hero';
@@ -8,6 +8,10 @@ import { Footer } from './sections/Footer';
 import { ThemeProvider } from '../hooks/useTheme';
 
 export default function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <ThemeProvider>
       <div
